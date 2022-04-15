d7f8576e0f1bf5d81396ed6e58eec9a63ae4e101
ooshima yuuki <toradosioosima@gmail.com>
2022-04-16 00:30:45 +0900

    Hello Worldを追加しました

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは　世界");
+
+		for (int i = 0; i < 20; i++) {
+			if (i < 10) {
+				System.out.println("Hello World");
+			} else {
+				System.out.println("Hello");
+			}
+		}
 	}
 
 }
